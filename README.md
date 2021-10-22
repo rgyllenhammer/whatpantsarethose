41ab40e61aae309b71e8db5332192dd91de249b8
Reese Gyllenhammer <reesegyllenhammer@reeses-mbp-2.myfiosgateway.com>
2021-10-21 21:31:39 -0400

    cleaned up files that are a part of whatpantsarethose_site

--- README.md
+++ README.md
@@ -10,10 +10,7 @@ A website of the instagram @whatpantsarethose to enable easy searching of both s
   - Install with `pip3 install insta-scrape`
 
 # Workflow
-The current workflow follows as this. Data is downloaded somehow in this file: https://github.com/rgyllenhammer/whatpantsarethose/blob/main/data/data_collection.py where you decide to attempt to download the whole instagram worth of data, or just append new posts that have been posted since last collection. That data is written to two files where it is then processed by this file: https://github.com/rgyllenhammer/whatpantsarethose/blob/main/data/data_processing.py which transforms the data, creates a dataframe, and writes data to an html file to display
+The current workflow follows as this. Data is downloaded somehow in this file: https://github.com/rgyllenhammer/whatpantsarethose/blob/main/data/data_collection.py where you either download the whole instagrams worth of data, or just append new posts that have been posted since last collection. That data is written to two files where it is then processed by this file: https://github.com/rgyllenhammer/whatpantsarethose/blob/main/data/data_processing.py which transforms the data, creates a dataframe, and writes it to a csv
 
-# Future work
-
-- Move site to hosting service that can hold a real backend
-- Search by both skater and pant
-- Automatically update with new posts
+# Website
+There is now a node site that hosts this data at https://github.com/rgyllenhammer/whatpantsarethose_site. This website will also eventually have an API so that it is not necessary for you to download data using this method. Perhaps eventually this method will be irrelevant, as data is moved to MongoDB
--- data/data_processing.py
+++ data/data_processing.py
@@ -2,20 +2,19 @@ import pandas as pd
 import numpy as np
 import re
 
-SKATER_REGEX = r'(@[\w|\.]+) '
-PANTS_REGEX = r'wearing (@?.+)\.'
-HTML_HEADER = '<html><head><link rel="stylesheet" href="main.css"></head></body><div class="header"><h1> whatpantsarethose </h1><p>created by @ranchgod</p><p>data from @whatpantsarethose</p></div>'
-HTML_FOOTER = '</body></html>'
+def generate_dataframe():
+    SKATER_REGEX = r'(@[\w|\.]+) '
+    PANTS_REGEX = r'wearing (@?.+)\.'
 
-data_urls = []
-data_captions = []
-with open('whatpantsarethose_urls.txt', 'r') as furls:
-    data_urls = furls.readlines()
+    data_urls = []
+    data_captions = []
+    with open('whatpantsarethose_urls.txt', 'r') as furls:
+        data_urls = furls.readlines()
 
-with open('whatpantsarethose_captions_raw.txt', 'r') as fcaptions:
-    data_captions = fcaptions.readlines()
+    with open('whatpantsarethose_captions_raw.txt', 'r') as fcaptions:
+        data_captions = fcaptions.readlines()
 
-df = pd.DataFrame(list(zip(data_urls, data_captions)), columns=['Post Url', 'Raw Caption'])
+    return pd.DataFrame(list(zip(data_urls, data_captions)), columns=['Post Url', 'Raw Caption'])
 
 def insert_skater(df):
     df['Skater'] = df['Raw Caption'].str.extract(SKATER_REGEX)
@@ -23,35 +22,10 @@ def insert_skater(df):
 def insert_pant(df):
     df['Pants'] = df['Raw Caption'].str.extract(PANTS_REGEX)
 
-def generate_skater_dictionary(df):
-    skater_dictionary = {}
-    for index, row in df.iterrows():
-        skater = row['Skater']
-        pants = row['Pants']
-        url = row['Post Url']
-
-        if skater not in skater_dictionary:
-            skater_dictionary[skater] = [{'Post Url': url, 'Pants': pants}]
-        else:
-            skater_dictionary[skater].append({'Post Url': url, 'Pants':pants})
-    return skater_dictionary
-
 def generate_csv(df):
     df.to_csv('pants_data.csv', index=False)
 
-def generate_html(skater_dictionary):
-    with open('../website/whatpantsarethose.html', 'r+') as htmlhandler:
-        htmlhandler.write(HTML_HEADER)
-
-        for skater in skater_dictionary:
-            htmlhandler.write('<h2> {} ({} hits)</h2>'.format(skater, len(skater_dictionary[skater])))
-            for data_dictionary in skater_dictionary[skater]:
-                htmlhandler.write('<p>{} <a href="{}"> {} </a></p>'.format(data_dictionary['Pants'], data_dictionary['Post Url'], data_dictionary['Post Url']))
-
-        htmlhandler.write(HTML_FOOTER)
-
+df = generate_dataframe()
 insert_skater(df)
 insert_pant(df)
-
-# generate_csv(df)
-# generate_html(generate_skater_dictionary(df))
+generate_csv(df)
